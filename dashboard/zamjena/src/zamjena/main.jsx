df8f4d1aed7ca6babf6286f31b1c607a3321a8b0
Nino <69417472+ninjock1981@users.noreply.github.com>
2025-04-20 14:05:37 +0200

    Delete dashboard/zamjena directory

--- dashboard/zamjena/src/zamjena/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AdminPanel from './AdminPanel';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<AdminPanel />);
