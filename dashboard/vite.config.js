6269fdb010640cc067eca7d5b05bae372ccdf9de
Nino <ninock1981@gmail.com>
2025-04-20 12:40:26 +0200

    🔥 Fix vite.config.js & enable frontend

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  }
+  base: './' // ✅ dodaj ovo za sigurni Vercel routing
 });
